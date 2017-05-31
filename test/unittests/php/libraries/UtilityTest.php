2405eebc09fda3bd7183c38625bcc469f4deb99e
Ng Justin <anyhowstep@hotmail.com>
2017-05-31 16:57:48 +0000

    renaming

--- test/unittests/php/libraries/UtilityTest.php
+++ test/unittests/php/libraries/UtilityTest.php
@@ -2,7 +2,7 @@
     require_once(__DIR__ . "/../../../../vendor/autoload.php");
     use PHPUnit\Framework\TestCase;
     
-    class Utility_TestCase extends TestCase {
+    class UtilityTest extends TestCase {
         public function testHelloWorld () {
             $this->assertEquals("Hello, world!", "Hello, " . "world!");
         }
