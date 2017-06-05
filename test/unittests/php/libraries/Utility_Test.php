96020d638a6fad1eb40cc39f59bdac35ca544ad3
Ng Justin <anyhowstep@hotmail.com>
2017-06-05 19:11:02 +0000

    test

--- test/unittests/php/libraries/Utility_Test.php
+++ test/unittests/php/libraries/Utility_Test.php
@@ -8,18 +8,13 @@
             $this->assertEquals($age["mon"], 1);
             $this->assertEquals($age["day"], 0);
             
-            for ($i=2; $i<=30; ++$i) {
+            for ($i=2; $i<=31; ++$i) {
                 $day = str_pad($i, 2, "0", STR_PAD_LEFT);
                 $age = Utility::calculateAge("2017-08-{$day}", "2017-09-01");
                 $this->assertEquals($age["year"], 0);
                 $this->assertEquals($age["mon"], 0);
-                $this->assertGreaterThan($age["day"], 0);
+                $this->assertEquals($age["day"], 31-$i);
             }
-            
-            $age = Utility::calculateAge("2017-08-31", "2017-09-01");
-            $this->assertEquals($age["year"], 0);
-            $this->assertEquals($age["mon"], 0);
-            $this->assertEquals($age["day"], 0);
         }
     }
 ?>
