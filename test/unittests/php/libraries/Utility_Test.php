fedb98d14a29b54c91c7d4866d8f2aad7cd94a3f
Ng Justin <anyhowstep@hotmail.com>
2017-06-05 18:47:15 +0000

    test

--- test/unittests/php/libraries/Utility_Test.php
+++ test/unittests/php/libraries/Utility_Test.php
@@ -2,8 +2,14 @@
     require_once __DIR__ . '/../../../../vendor/autoload.php';
 
     class Utility_Test extends PHPUnit_Framework_TestCase {
-        function testAssert () {
-            $this->assertEquals(true, false);
+    
+        function test_calculateAge () {
+            $age = Utility::calculateAge("2017-02-28", "2017-03-01");
+            $this->assertEquals($age, [
+                "year"=>0,
+                "mon"=>0,
+                "day"=>1
+            ]);
         }
     }
 ?>
