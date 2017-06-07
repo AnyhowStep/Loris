33ae8055a3d965ee0bfb07444e152493a5ffe7a5
Ng Justin <anyhowstep@hotmail.com>
2017-06-07 23:36:54 +0000

    test

--- test/test-case-set-up.sh
+++ test/test-case-set-up.sh
@@ -1,3 +1,3 @@
 mysql -e 'CREATE DATABASE LorisTest'
-cat SQL/0000-00-00-schema.sql | mysql LorisTest
+cat SQL/0000-00-00-schema.sql | mysql -h localhost LorisTest
 mysql -e 'SELECT * FROM LorisTest.users'
